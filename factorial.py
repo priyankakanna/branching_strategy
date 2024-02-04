ae0f41a0eaf191f92aa92562e499ea7f6eaee1df
priyankakanna <priyankakanna.01@gmail.com>
2024-02-04 23:36:09 +0200

    Add factorial feature

---
+++ factorial.py
@@ -0,0 +1,9 @@
+# factorial.py
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print("Factorial of 5:", factorial(5))
+# Make changes to factorial.py
